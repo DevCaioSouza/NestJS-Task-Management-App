a7a7c1efa14857581a5ee20a9d384bbbf9a898a4
DevCaioSouza <caiocbahia@hotmail.com>
2024-06-23 16:59:50 -0300

    Add Tasks Model + service/controller methods

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // Open-Closed principle: Open for extension, closed for modification 
+  // tasks não pode ser alterado por componentes externos
+  private tasks = []
+
+  getAllTasks(){
+    return this.tasks
+  }
+
+}
