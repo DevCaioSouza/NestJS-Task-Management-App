347af4b251b2513b3681e0ac56122f05627915f5
DevCaioSouza <caiocbahia@hotmail.com>
2024-06-23 19:00:47 -0300

    Add createTask method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { Task, TaskStatus } from './tasks.model'
+import * as uuid from 'uuid'
 
 @Injectable()
 export class TasksService {
   // Open-Closed principle: Open for extension, closed for modification 
   // tasks não pode ser alterado por componentes externos
-  private tasks = []
+  private tasks: Task[] = []
 
-  getAllTasks(){
+  getAllTasks(): Task[] {
     return this.tasks
+  }
+
+  createTask(title: string, description: string) {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    }
+
+    this.tasks.push(task)
   }
 
 }
