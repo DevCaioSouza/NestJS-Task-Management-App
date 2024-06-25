ae7101224acfff26f5843ed6ea7f358bda82654f
DevCaioSouza <caiocbahia@hotmail.com>
2024-06-25 17:51:58 -0300

    Add createTask method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { Task, TaskStatus } from './tasks.model'
-import * as uuid from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 @Injectable()
 export class TasksService {
@@ -12,15 +12,21 @@ export class TasksService {
     return this.tasks
   }
 
-  createTask(title: string, description: string) {
+  createTask(title: string, description: string): Task {
     const task: Task = {
-      id: uuid(),
+      id: uuidv4(),
       title,
       description,
       status: TaskStatus.OPEN
     }
 
     this.tasks.push(task)
+    return task
+
+    //quando criamos um recurso numa RestAPI, é de boa prática retorná-lo na função
+    //isso é ótimo para o dev frontend, pois:
+    // 1- Reduz o load do app
+    // 2- Quando o objeto é exibido p/ o usuário na tela, não é necessário fazer uma nova requisição
   }
 
 }
