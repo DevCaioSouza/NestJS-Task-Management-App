39c441f41eb38a25a92432b239d3da3e469194bb
DevCaioSouza <caiocbahia@hotmail.com>
2024-07-05 17:35:30 -0300

    Refactor typeORM Module main call

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,24 @@ import { Module } from '@nestjs/common'
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { DataSource } from 'typeorm';
+import { Task } from './tasks/task.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'taskmanagement',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true
+    }),
     TasksModule
   ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,27 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const typeORMConfig: TypeOrmModuleOptions = {
+// ALERT: FILE KEPT UNDER REFERENCE PURPOSE. IT'LL SOON BE DELETED
+
+import { DataSource } from "typeorm";
+
+export const typeORMConfig = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
   database: 'taskmanagement',
-  entities: [__dirname + '/../**/*.entity.ts'],
+  entities: [__dirname + 'dist/src/**/*.entity{.ts,.js}'],
   synchronize: true
-}
+})
+
+typeORMConfig.initialize()
+  .then(() => {
+    console.log(`Data Source has been initialized`);
+  })
+  .catch((err) => {
+    console.error(`Data Source initialization error`, err);
+  })
+
+
+// ["dist/src/**/*.entity{.ts,.js}"]
